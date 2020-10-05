82053bddbaef1660e27399f5cbac1cdbd0b11b3f
Jose A. Casado <jsedoviolt@gmail.com>
2020-10-05 22:32:37 +0200

    add rover object, complete turnLeft function, create command function with a switch statement

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // Rover object goes here:
+const rover = { direction: 'N', x: '0', y: '0'};
 
 // ======================
 function turnLeft(rover) {
-  console.log('turnLeft was called!');
+  if (rover.direction === 'N') {
+    rover.direction = 'W';
+    console.log(`The Rover direction is ${rover.direction}`);
+  } else if (rover.direction === 'W') {
+    rover.direction = 'S';
+    console.log(`The Rover direction is ${rover.direction}`);
+  } else if (rover.direction === 'S') {
+    rover.direction = 'E';
+    console.log(`The Rover direction is ${rover.direction}`);
+  } else {
+    rover.direction = 'N';
+    console.log(`The Rover direction is ${rover.direction}`);
+  }
 }
 
 function turnRight(rover) {
@@ -12,3 +25,23 @@ function turnRight(rover) {
 function moveForward(rover) {
   console.log('moveForward was called');
 }
+
+function command(rover, orders) {
+  for (let i = 0; i < orders.length; i++) {
+    let order = orders[i];
+    switch (order) {
+      case 'l': // left
+        turnLeft(rover);
+        break;
+      case 'r': // rigth
+        turnRight(rover);
+        break;
+      case 'f': // fordward
+        moveForward(rover);
+        break;
+    }
+  }
+  console.log(`The Rover is facing ${rover.direction}`);
+}
+
+command(rover, 'rlllf');
