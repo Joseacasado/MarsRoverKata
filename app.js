a2965db2c074c7e14bbe0881bb836f3f15af5c63
Jose A. Casado <jsedoviolt@gmail.com>
2020-10-05 15:44:11 +0200

    add starter code

---
+++ app.js
@@ -0,0 +1,14 @@
+// Rover object goes here:
+
+// ======================
+function turnLeft(rover) {
+  console.log('turnLeft was called!');
+}
+
+function turnRight(rover) {
+  console.log('turnRight was called!');
+}
+
+function moveForward(rover) {
+  console.log('moveForward was called');
+}
